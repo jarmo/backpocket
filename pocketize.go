b75bbfd404de0dff2240ab73a0f6c6179609e7a7
Jarmo Pertman <jarmo@jarmopertman.com>
2020-02-01 14:01:59 +0200

    Extract styles into separate file

--- pocketize.go
+++ pocketize.go
@@ -85,7 +85,7 @@ func articleWithStyling(uri *url.URL, article readability.Article) string {
 		</html>
 		`,
 		article.Title,
-		styles(),
+		Styles(),
 		uri.String(), article.Title,
 		archivedAt.Format("January 2, 2006"),
 		article.Image,
@@ -111,56 +111,7 @@ func addressFile(address *url.URL, err error) string {
 	  </header>
 	</body>
 </html>
-	`, address.String(), styles(), address.String(), address.String(), err)
-}
-
-func styles() string {
-	return `
-		body {
-			font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-			font-size: 1.3em;
-		}
-		
-		img {
-			max-width: 100%;
-		}
-
-		body > header {
-			width: 80%;
-			margin: 0 auto;
-			margin-bottom: 10em;
-			text-align: center;
-		}
-
-		body > header small, body > header .archived-at {
-			color: gray;
-		}
-
-		body > header .archived-at {
-			font-size: 50%;
-		}
-
-		body > header img {
-			margin-bottom: 1em;
-		}
-
-		body > header figcaption {
-			font-size: 80%;
-		}
-
-		body > article {
-			width: 45%;
-			margin: 0 auto;
-		}
-
-		a {
-			text-decoration: none;
-		}
-
-		a:hover {
-			text-decoration: underline;
-		}
-`
+	`, address.String(), Styles(), address.String(), address.String(), err)
 }
 
 func byline(article readability.Article) string {
---
+++ styles.go
@@ -0,0 +1,51 @@
+package main
+
+func Styles() string {
+	return `
+		body {
+			font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+			font-size: 1.3em;
+		}
+		
+		img {
+			max-width: 100%;
+		}
+
+		body > header {
+			width: 80%;
+			margin: 0 auto;
+			margin-bottom: 10em;
+			text-align: center;
+		}
+
+		body > header small, body > header .archived-at {
+			color: gray;
+		}
+
+		body > header .archived-at {
+			font-size: 50%;
+		}
+
+		body > header img {
+			margin-bottom: 1em;
+		}
+
+		body > header figcaption {
+			font-size: 80%;
+		}
+
+		body > article {
+			width: 45%;
+			margin: 0 auto;
+		}
+
+		a {
+			text-decoration: none;
+		}
+
+		a:hover {
+			text-decoration: underline;
+		}
+`
+}
+
