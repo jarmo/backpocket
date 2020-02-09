165d9e39c8cde3cc602096676f28d26cfaa9cbac
Jarmo Pertman <jarmo@jarmopertman.com>
2020-02-09 22:01:48 +0200

    Create better title for extracted contents using URL path when title is not accessible

--- article/path.go
+++ article/path.go
@@ -14,7 +14,7 @@ import (
 const RootDir = "backpocket-contents"
 
 func ReadableArticleFilePath(address *url.URL, article readability.Article) string {
-	return path.Join(RootDir, fmt.Sprintf("%s-%s-%s.html", time.Now().Format("2006-01-02"), formattedTitle(article.Title), formattedHost(address)))
+	return path.Join(RootDir, fmt.Sprintf("%s-%s.html", time.Now().Format("2006-01-02"), formattedTitle(article.Title)))
 }
 
 func NonReadableArticleFilePath(address *url.URL) string {
@@ -22,7 +22,7 @@ func NonReadableArticleFilePath(address *url.URL) string {
 }
 
 func NonHTMLContentFilePath(address *url.URL, contentType string) string {
-	return path.Join(RootDir, fmt.Sprintf("%s-%s.%s", time.Now().Format("2006-01-02"), formattedTitle(strings.ReplaceAll(path.Base(address.Path), path.Ext(address.Path), "")), extension(contentType)))
+	return path.Join(RootDir, fmt.Sprintf("%s-%s.%s", time.Now().Format("2006-01-02"), formattedTitle(titleFromPath(address)), extension(contentType)))
 }
 
 func extension(contentType string) string {
@@ -33,10 +33,14 @@ func extension(contentType string) string {
 	}
 }
 
+func titleFromPath(url *url.URL) string {
+	return strings.ReplaceAll(path.Base(url.Path), path.Ext(url.Path), "")
+}
+
 func formattedTitle(title string) string {
-	replaceInvalidCharactersRegexp := regexp.MustCompile("[<>:\"'/\\|?*=;.%^ ]")
+	replaceInvalidCharactersRegexp := regexp.MustCompile("[<>:\"'/\\|?*=;.%,^]")
 	replaceDuplicateAdjacentDashesRegexp := regexp.MustCompile("-{2,}")
-	return replaceDuplicateAdjacentDashesRegexp.ReplaceAllString(replaceInvalidCharactersRegexp.ReplaceAllString(strings.ReplaceAll(title, "&", "and"), "-"), "-")
+	return replaceDuplicateAdjacentDashesRegexp.ReplaceAllString(replaceInvalidCharactersRegexp.ReplaceAllString(strings.ReplaceAll(strings.ReplaceAll(title, " ", "-"), "&", "and"), ""), "-")
 }
 
 func formattedHost(address *url.URL) string {
