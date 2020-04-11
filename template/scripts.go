4d90b1d3b266972460df800ee1beaf248fd51a4a
Jarmo Pertman <jarmo@jarmopertman.com>
2020-04-11 19:56:51 +0300

    Fix iframe urls without scheme to be always using https so that opening an archived file over file protocol would work too

--- template/scripts.go
+++ template/scripts.go
@@ -7,5 +7,10 @@ document.querySelectorAll("img").forEach(img => {
   img.removeAttribute("width")
   img.removeAttribute("height")
 })
+
+document.querySelectorAll("iframe").forEach(iframe => {
+	const iframeSrc = iframe.getAttribute("src")
+	if (iframeSrc.startsWith("//")) iframe.setAttribute("src", "https:" + iframeSrc)
+})
 `
 }
