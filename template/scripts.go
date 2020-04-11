58e6ead26b821db7c3488b0ab6831dc06a1f33c6
Jarmo Pertman <jarmo@jarmopertman.com>
2020-04-11 20:13:19 +0300

    Remove all images with JavaScript which were failed to be replaced by base64 data image source

--- template/scripts.go
+++ template/scripts.go
@@ -2,15 +2,21 @@ package template
 
 func Scripts() string {
 	return `
-document.querySelectorAll("img").forEach(img => {
-  img.removeAttribute("srcset")
-  img.removeAttribute("width")
-  img.removeAttribute("height")
-})
+document.addEventListener("DOMContentLoaded", () => {
+	document.querySelectorAll("img").forEach(img => {
+		if (img.naturalWidth === 0) {
+			img.remove()
+		} else {
+			img.removeAttribute("srcset")
+			img.removeAttribute("width")
+			img.removeAttribute("height")
+		}
+	})
 
-document.querySelectorAll("iframe").forEach(iframe => {
-	const iframeSrc = iframe.getAttribute("src")
-	if (iframeSrc.startsWith("//")) iframe.setAttribute("src", "https:" + iframeSrc)
+	document.querySelectorAll("iframe").forEach(iframe => {
+		const iframeSrc = iframe.getAttribute("src")
+		if (iframeSrc.startsWith("//")) iframe.setAttribute("src", "https:" + iframeSrc)
+	})
 })
 `
 }
