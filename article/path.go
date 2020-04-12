810a80c566aedaafa854ced2fef672258fbca68f
Jarmo Pertman <jarmo@jarmopertman.com>
2020-04-12 18:50:07 +0300

    Calculate suffix based on the input address so that it is possible to regenerate same file when needed

--- article/path.go
+++ article/path.go
@@ -1,28 +1,28 @@
 package article
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
+	"math"
 	"net/url"
 	"path/filepath"
 	"regexp"
 	"strings"
-	"time"
-	"math/rand"
-	"math"
 
 	readability "github.com/go-shiori/go-readability"
 )
 
 func ReadableArticleFilePath(storageDir string, params ArticleParams, article readability.Article) string {
-	return filepath.Join(storageDir, fmt.Sprintf("%s-%s-%s.html", params.ArchivedAt.Format("2006-01-02"), titleFromArticleOrPath(article, params.Url), randomSuffix()))
+	return filepath.Join(storageDir, fmt.Sprintf("%s-%s-%s.html", params.ArchivedAt.Format("2006-01-02"), titleFromArticleOrPath(article, params.Url), suffix(params.Url)))
 }
 
 func NonReadableArticleFilePath(storageDir string, params ArticleParams) string {
-	return filepath.Join(storageDir, fmt.Sprintf("%s-%s-%s.html", params.ArchivedAt.Format("2006-01-02"), titleFromUrl(params.Url), randomSuffix()))
+	return filepath.Join(storageDir, fmt.Sprintf("%s-%s-%s.html", params.ArchivedAt.Format("2006-01-02"), titleFromUrl(params.Url), suffix(params.Url)))
 }
 
 func NonHTMLContentFilePath(storageDir string, params ArticleParams, contentType string) string {
-	return filepath.Join(storageDir, fmt.Sprintf("%s-%s-%s.%s", params.ArchivedAt.Format("2006-01-02"), titleFromUrl(params.Url), randomSuffix(), extension(contentType)))
+	return filepath.Join(storageDir, fmt.Sprintf("%s-%s-%s.%s", params.ArchivedAt.Format("2006-01-02"), titleFromUrl(params.Url), suffix(params.Url), extension(contentType)))
 }
 
 func extension(contentType string) string {
@@ -61,14 +61,14 @@ func formattedTitle(title string) string {
 				replaceInvalidCharactersRegexp.ReplaceAllString(
 					strings.ReplaceAll(
 						strings.ReplaceAll(title,
-						" ", "-"),
-					"&", "and"),
+							" ", "-"),
+						"&", "and"),
+					""),
 				""),
-			""),
+			"-"),
 		"-"),
-	"-"),
-	"."))
-	
+		"."))
+
 	maxTitleLength := int(math.Min(64, float64(len(formattedTitle))))
 	return formattedTitle[:maxTitleLength]
 }
@@ -77,12 +77,8 @@ func formattedHost(address *url.URL) string {
 	return strings.ReplaceAll(address.Hostname(), ".", "-")
 }
 
-func randomSuffix() string {
-	rand.Seed(time.Now().UnixNano())
-	const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	b := make([]byte, 8)
-	for i := range b {
-			b[i] = characters[rand.Intn(len(characters))]
-	}
-	return string(b)
+func suffix(address *url.URL) string {
+	hash := sha1.New()
+	hash.Write([]byte(address.String()))
+	return hex.EncodeToString(hash.Sum(nil))[:8]
 }
